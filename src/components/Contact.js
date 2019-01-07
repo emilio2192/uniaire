94b3521510e5d276368522f2f452b2fd6a99f889
Emilio Reyes <ereyes@corporacionmastergroup.com>
2019-01-07 16:23:51 -0600

    style contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,42 @@
 import React, {Component} from 'react';
+import '../_css/contact.css';
 
-
-export default class Contact extends Component{
+export default class Contact extends Component {
     constructor(props) {
         super(props);
     }
+
     render() {
         return [
-            <h1>Contact</h1>
+            <h1>Contáctanos</h1>,
+            <div className="form-container">
+                <div className="row-input">
+                    <label>Nombre Completo:</label>
+                    <input type="text" placeholder="Nombre Completo"/>
+                </div>
+                <div className="row-input">
+                    <label>Asunto:</label>
+                    <input type="text" placeholder="Asunto"/>
+                </div>
+                <div className="row-input">
+                    <label>Mensaje:</label>
+                    <textarea type="text" placeholder="Mensaje"/>
+                </div>
+                <div className="row-input">
+                    <div className="container-button">
+                        <a href="#" className="fancy-button medium rounded alizarin zoom expand">
+                            Enviar
+                        </a>
+                    </div>
+                </div>
+            </div>,
+            <div className="location-container">
+                Sede Central: Mixco<br/>
+                Telefonos: 5431-2346 / 2315-8844<br/>
+                {'Correo: info@uniaire.com.gt'}
+            </div>
+
+
         ];
     }
 
